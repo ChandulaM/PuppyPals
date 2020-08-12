cc3d002d729169c0c11f123c3d06d42ee0c10a25
Chandula <chandula.madigasekara@gmail.com>
2020-08-12 07:40:25 +0530

    Initial layouts for my part

---
+++ app/src/main/java/com/example/puppypals/ChangePassword.java
@@ -0,0 +1,16 @@
+package com.example.puppypals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
