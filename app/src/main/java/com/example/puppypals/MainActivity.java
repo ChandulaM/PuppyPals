394526343cdddd0102b3c1127b3aadcf2498ec2a
Nadun <dilhannadun777@gmail.com>
2020-08-12 22:31:49 +0530

    my commit 01 - add breed info

---
+++ app/src/main/java/com/example/puppypals/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.puppypals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
