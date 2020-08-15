43b07f3d07ee6a5bdcd522cd9bae19564b8ffbb6
Nadun <dilhannadun777@gmail.com>
2020-08-15 23:31:37 +0530

    Can merge...

--- app/src/main/java/com/example/puppypals/botNav.java
+++ app/src/main/java/com/example/puppypals/botNav.java
@@ -1,14 +1,6 @@
 package com.example.puppypals;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 
-import android.os.Bundle;
-
-public class botNav extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bot_nav);
-    }
+public class botNav extends Activity {
 }
