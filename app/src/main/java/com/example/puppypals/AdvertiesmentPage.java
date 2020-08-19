e071cf59206bea70dd79316953272ceaca5a9796
Asanka98 <asanka.asb12345@gmail.com>
2020-08-19 00:09:02 -0700

    initial-commit-for-advertiesment-pages

---
+++ app/src/main/java/com/example/puppypals/AdvertiesmentPage.java
@@ -0,0 +1,14 @@
+package com.example.puppypals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdvertiesmentPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advertiesment_page);
+    }
+}
